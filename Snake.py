1b6e89c939550431381691e99ac0ecc8a808406f
Tomas <tmalavassi@gmail.com>
2022-05-20 21:50:31 -0600

    Agregue la estructura de pasos al objeto maze

--- Snake.py
+++ Snake.py
@@ -3,9 +3,13 @@ from libs.pymaze import maze
 m=maze()
 m.CreateMaze(loadMaze=r"D:\Dropbox\Maestria\IA\Proyecto\Codigo\Snake\mapa.csv")
 
-m.snakeDelay = 1
+MAX_STEPS = m.cols * m.rows
+
+
+m.snakeDelay = 1000
 m.caminoAleatorio = True
 m.liberacion = False
+m.steps = MAX_STEPS
 
 m.configurarSnake()
 
